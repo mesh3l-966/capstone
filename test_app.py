6bf920e8e11494d7d4bad9dabb73579bdc46e9e3
mesh3l-966 <mesh3l-966@hotmail.com>
2020-09-07 15:15:06 +0300

    change in test_app to database_path = os.environ['DATABASE_URL']

--- test_app.py
+++ test_app.py
@@ -15,9 +15,10 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "capstonetest"
+        #self.database_name = "capstonetest"
 
-        self.database_path = "postgresql://{}/{}".format('postgres:laug999@localhost:5432', self.database_name)
+        #self.database_path = "postgresql://{}/{}".format('postgres:laug999@localhost:5432', self.database_name)
+        database_path = os.environ['DATABASE_URL']
         setup_db(self.app, self.database_path)
         self.errors = errors(self.app)
         self.executive_producer = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ii1mbC04UmhnLXIwZVdNZWotdXZzVyJ9.eyJpc3MiOiJodHRwczovL21lc2hhbC5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NWVkZWJmYTFhMzZlYjIwMDE5N2QyOWMxIiwiYXVkIjoiY2Fwc3RvbmUiLCJpYXQiOjE1OTk0MjgyODYsImV4cCI6MTU5OTUxNDY4NiwiYXpwIjoiZHlINmxDVGFsUnB3T2ZhWmZiUVY2MlFZeUhFVWx0TnIiLCJzY29wZSI6IiIsInBlcm1pc3Npb25zIjpbImRlbGV0ZTphY3RvciIsImRlbGV0ZTptb3ZpZSIsImdldDphY3RvcnMiLCJnZXQ6bW92aWVzIiwicGF0Y2g6YWN0b3IiLCJwYXRjaDptb3ZpZSIsInBvc3Q6YWN0b3IiLCJwb3N0Om1vdmllIl19.25-jXgsGi1eS5L0VuSBiMNIoRSQsP2gHQR1onNCcBPl_EpsqsmLRd1R1zIfybDWNxkl65HRbfdVrFtqHxgPRgDNKthy7vUZxcU5CXHTy-e6dKInmFX-fBIhmyrq7T9SY6lIw5wb7TvpWDe_u9G1PxxsljOSrU0eW6IgWxE2rW0vrRwSpY9THlmIvXDLBT1-sQ8yZCZE6HIRcNxI3KHMTM_i8Xs1hQixxDW_K0SDwGUUK0qlCgUa8LqdgIB96ZbII9WxIJKFd9ZFekx1db2qaUZbOurm0Lyi_wKBA1M51sw8GIjLXZWb4AmjisxBzHBV9oMsJGI0ILQY8hKtVqklpug'
